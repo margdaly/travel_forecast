4fb499d936f6667ad6b573de1caa985ade7df088
margdaly <margaret.daly@gmail.com>
2023-11-30 19:50:27 -0700

    Add necessary dependencies for code coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,20 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec'
+  add_filter '/config'
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
+require 'support/factory_bot'
+require 'support/helper_methods'
+require "capybara_table/rspec"
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'simplecov'
 SimpleCov.start 'rails'
@@ -24,6 +34,8 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Capybara::DSL
+
+  WebMock.disable_net_connect!(allow_localhost: true)
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
